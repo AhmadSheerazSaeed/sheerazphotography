090bf2e58dd2544620dbc8545c3ab2d10acb0152
AhmadSheerazSaeed <khera724@gmail.com>
2022-07-12 20:28:03 +0200

    This is a commit

---
+++ script/togglerMenu.js
@@ -0,0 +1,5 @@
+function togglerFunction() {
+  document.getElementById("toggler").classList.toggle("togglerChange");
+  document.getElementById("navBar1").classList.toggle("navBar1Toggle");
+  document.getElementById("nav-bg").classList.toggle("change-bg");
+}
