1c69d2f44646f20d700b6e4b10dfb288cd7bd384
AhmadSheerazSaeed <khera724@gmail.com>
2022-07-13 10:34:14 +0200

    Changes 13 07 2022

--- script/togglerMenu.js
+++ script/togglerMenu.js
@@ -1,5 +1,6 @@
 function togglerFunction() {
   document.getElementById("toggler").classList.toggle("togglerChange");
   document.getElementById("navBar1").classList.toggle("navBar1Toggle");
-  document.getElementById("nav-bg").classList.toggle("change-bg");
+
+  /*document.getElementById("nav-bg").classList.toggle("change-bg");*/
 }
